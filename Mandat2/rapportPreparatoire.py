e60bbb65b43350c274d77347cf842ca12236cd4e
JonathanLG64 <jonathan.lafreniere99@gmail.com>
2022-10-25 11:51:53 -0400

    colormap

--- Mandat2/rapportPreparatoire.py
+++ Mandat2/rapportPreparatoire.py
@@ -1,12 +1,6 @@
 import numpy as np
 from scipy import signal, io
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from pylab import *
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.ndimage.filters import gaussian_filter
-import seaborn as sns
-import pandas as pd
 
 def cont_res(x, y):
     dx = abs(x[0] - x[1])
@@ -16,9 +10,9 @@ def cont_res(x, y):
     contrast = np.max(y) - width[1][0]
     return [contrast, resolution]
 
-carre = io.loadmat(r'DemiCercle\correl_demi_cercle.mat')
-position = io.loadmat(r'Croco\position.mat')
-x_plot = io.loadmat(r'Croco\x_plot.mat')
+carre = io.loadmat(r'Carree\carre.mat')
+position = io.loadmat(r'Carree\position.mat')
+x_plot = io.loadmat(r'Carree\x_plot.mat')
 
 xSource = x_plot['x_plot'][0]
 correl = carre['correl']
@@ -29,11 +23,16 @@ z =  np.array([cont_res(xSource,y) for y in correl])
 
 cont = z[:, 0]
 res = z[:, 1]
-x = pos[:, 0]
-y = pos[:, 1]
+x = pos[:, 0]*1e3
+y = pos[:, 1]*1e3
 
-X, Y = np.meshgrid(y, x)
-Z = np.reshape(cont, (21, 21))
+x = x -100
+y = y - 100
+X, Y = np.meshgrid(x, y)
+Z = np.reshape(cont, (21, 21)).T
 
 plt.imshow(Z, extent=[min(x), max(x), min(y), max(y)])
+plt.xlabel('x - position [mm]')
+plt.ylabel('y - position [mm]')
+plt.colorbar()
 plt.show()
