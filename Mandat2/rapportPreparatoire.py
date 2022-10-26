6b0c864ace3e303f3a7e2b31b82dc3b152270f8d
JonathanLG64 <jonathan.lafreniere99@gmail.com>
2022-10-25 23:17:09 -0400

    commentaires

--- Mandat2/rapportPreparatoire.py
+++ Mandat2/rapportPreparatoire.py
@@ -2,16 +2,21 @@ import numpy as np
 from scipy import signal, io
 import matplotlib.pyplot as plt
 
+# Calcule le contraste et la résolution d'un signal entré
 def cont_res(x, y):
     dx = abs(x[0] - x[1])
-    peak, _ = signal.find_peaks(y, distance=10e3)
+    # Trouve le plus grand pic dans le signal
+    peak, _ = signal.find_peaks(y, distance=1e3)
+    # mesure la largeur à mi-hauteur pour calculer la résolution
     width = signal.peak_widths(y, peak, rel_height=0.5)[0][0]
     resolution = width*dx
+    # Fait la moyenne des points qui sont considérés à l'extérieur du pic
     contwidth = signal.peak_widths(y, peak, rel_height=0.8)
     left = int(contwidth[2][0])
     right = int(contwidth[3][0])
+    # calcule la moyenne de ces points comme baseline
     base = (np.mean(y[x < x[left]]) + np.mean(y[x > x[right]]))/2
-    contrast = np.max(y) / base
+    contrast = np.max(y) - base
     if np.isnan(contrast):
         contrast = 0
     return [contrast, resolution]
@@ -37,7 +42,7 @@ y = y - 100
 X, Y = np.meshgrid(x, y)
 Zres = np.reshape(res, (21, 21)).T*1e3
 Zcont = np.reshape(cont, (21, 21)).T
-
+# affichage des données dans des heatmaps
 plt.imshow(Zres, extent=[min(x), max(x), min(y), max(y)])
 plt.xlabel('x - position [mm]')
 plt.ylabel('y - position [mm]')
